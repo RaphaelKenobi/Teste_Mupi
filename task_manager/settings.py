3b30d7172f5c28ea46b92c7c13a4031e66c65646
Raphael Pereira da Silva piloneto <rafasilvapiloneto@gmail.com>
2024-08-27 02:10:18 -0300

    base

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -128,8 +128,12 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
