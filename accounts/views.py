612ea72c9332feac9d344e8d8c3d5c9ca2d5f002
Raphael Pereira da Silva piloneto <rafasilvapiloneto@gmail.com>
2024-08-27 17:59:15 -0300

    teste

--- accounts/views.py
+++ accounts/views.py
@@ -20,3 +20,6 @@ def register(request):
             print('invalid registration details')
 
     return render(request, "registration/register.html", {"form": form})
+
+def data(request):
+    return render(request, "registration/data.html")
